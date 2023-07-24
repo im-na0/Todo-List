45269f2f464501a361df4d008bc860d06cc4d5ad
imnayoung <myblu1008@gmail.com>
2023-07-25 03:49:12 +0900

    Feat: remove button

--- js/main.js
+++ js/main.js
@@ -11,75 +11,90 @@ function getUniqueID() {
  * Create Input & add button
  */
 const inputEl = document.getElementById('input') // 지역변수로 바꾸기!!!!!!!!!
-const addBtnEl = document.getElementById('addBtn') // 지역변수로 바꾸기!!!!!!!
-
+const addBtnEl = document.getElementById('addBtn')
+const listEl = document.querySelector('.todo-list') // 지역변수로 바꾸기!!!!!!!
 // Add button Event
 addBtnEl.addEventListener('click', () => {
-  onClick()
+  addTask()
 })
-
 // EnterKey Event
 inputEl.addEventListener('keydown', (e) => {
   if (e.key === 'Enter') {
     if (e.isComposing) return // keydown 이슈방지
-    onClick()
+    addTask()
   }
 })
 
-function onClick() {
+/**
+ * Add Task
+ */
+function addTask() {
   // 공백 입력 시 경고창
   if (inputEl.value === '') {
     alert('문자를 입력해주세요!')
     return
   }
-  const listEl = document.querySelector('.todo-list')
   const taskEl = document.createElement('li')
-  // 할 일 추가 + 고유 번호 클래스명
-  const className = getUniqueID()
-  taskEl.classList.add(className)
+  // 할 일 추가
   taskEl.textContent = inputEl.value
-
   listEl.appendChild(taskEl)
   // input 초기화
   inputEl.value = null
-
-  // checkbox생성 함수호출
-  AddCheckBox(taskEl)
-
   // remove button생성 함수호출
-  AddDltBtn(taskEl, listEl, className)
+  addDltBtn(taskEl)
+  // checkbox생성 함수 호출
+  CtlCheckbox(taskEl)
+  // Save data
+  saveData()
 }
 
 /**
- * Checkbox + label
+ * Checkbox control
  */
-function AddCheckBox(taskEl) {
-  const checkBox = document.createElement('input')
-  const label = checkBox.createElement('label')
-  // Id 추가 & 속성 추가
-  const idName = getUniqueID()
-  checkBox.setAttribute('id', idName)
-  checkBox.setAttribute('type', 'checkbox')
-  label.setAttribute('for', 'cbtest')
-  taskEl.prepend(checkBox)
-  checkBox.appendChild(label)
+function CtlCheckbox(taskEl) {
+  taskEl.addEventListener('click', (e) => {
+    if (e.target.tagName === 'LI') {
+      e.target.classList.toggle('checked') // toggle로 checked 감시
+      saveData()
+    }
+  })
 }
 
 /**
  * Remove button
  */
-function AddDltBtn(taskEl, listEl, ClassName) {
+function addDltBtn(taskEl) {
   // remove button 생성
-  const dltBtn = document.createElement('input')
-
-  dltBtn.setAttribute('type', 'button')
-  dltBtn.setAttribute('value', 'x')
-
-  taskEl.append(dltBtn)
-
-  // Delete Elements Event
-  dltBtn.addEventListener('click', () => {
-    const dltEl = listEl.querySelector(`.${ClassName}`)
-    dltEl.remove()
+  let xIcon = document.createElement('i')
+  xIcon.setAttribute('class', 'xi-close')
+  taskEl.append(xIcon)
+  // remove event
+  xIcon.addEventListener('click', (e) => {
+    if (e.target.tagName === 'I') {
+      e.target.parentElement.remove()
+      saveData()
+    }
   })
 }
+
+// 데이터를 JSON 형식으로 저장
+function saveData() {
+  const data = {
+    listHTML: listEl.innerHTML,
+    otherData: 'some other data',
+    // ... 추가적인 데이터 저장
+  }
+  localStorage.setItem('data', JSON.stringify(data))
+}
+
+// JSON 데이터를 가져와서 화면에 보여주기
+function showTask() {
+  const data = JSON.parse(localStorage.getItem('data'))
+  if (data) {
+    listEl.innerHTML = data.listHTML
+    // 다른 데이터도 복원할 수 있음
+    // someOtherElement.textContent = data.otherData;
+  }
+}
+
+showTask()
