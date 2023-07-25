69f3ca0924d7a4ec9cc2913672977349664a6044
imnayoung <myblu1008@gmail.com>
2023-07-25 10:11:03 +0900

    Feat: localstorage

--- js/main.js
+++ js/main.js
@@ -1,23 +1,9 @@
-/**
- * Unique Number Counter
- */
-let counter = 0
-
-function getUniqueID() {
-  return `id-${counter++}`
-}
-
-/**
- * Create Input & add button
- */
-const inputEl = document.getElementById('input') // 지역변수로 바꾸기!!!!!!!!!
+const inputEl = document.getElementById('input')
 const addBtnEl = document.getElementById('addBtn')
-const listEl = document.querySelector('.todo-list') // 지역변수로 바꾸기!!!!!!!
-// Add button Event
-addBtnEl.addEventListener('click', () => {
-  addTask()
-})
-// EnterKey Event
+const listEl = document.querySelector('.todo-list')
+
+addBtnEl.addEventListener('click', addTask)
+
 inputEl.addEventListener('keydown', (e) => {
   if (e.key === 'Enter') {
     if (e.isComposing) return // keydown 이슈방지
@@ -25,50 +11,38 @@ inputEl.addEventListener('keydown', (e) => {
   }
 })
 
-/**
- * Add Task
- */
 function addTask() {
-  // 공백 입력 시 경고창
   if (inputEl.value === '') {
     alert('문자를 입력해주세요!')
     return
   }
+
   const taskEl = document.createElement('li')
-  // 할 일 추가
-  taskEl.textContent = inputEl.value
+  const taskTxt = document.createElement('span')
+  taskTxt.textContent = inputEl.value
   listEl.appendChild(taskEl)
-  // input 초기화
+  taskEl.appendChild(taskTxt)
   inputEl.value = null
-  // remove button생성 함수호출
+
   addDltBtn(taskEl)
-  // checkbox생성 함수 호출
   CtlCheckbox(taskEl)
-  // Save data
   saveData()
 }
 
-/**
- * Checkbox control
- */
 function CtlCheckbox(taskEl) {
   taskEl.addEventListener('click', (e) => {
     if (e.target.tagName === 'LI') {
-      e.target.classList.toggle('checked') // toggle로 checked 감시
+      e.target.classList.toggle('checked')
       saveData()
     }
   })
 }
 
-/**
- * Remove button
- */
 function addDltBtn(taskEl) {
-  // remove button 생성
   let xIcon = document.createElement('i')
   xIcon.setAttribute('class', 'xi-close')
   taskEl.append(xIcon)
-  // remove event
+
   xIcon.addEventListener('click', (e) => {
     if (e.target.tagName === 'I') {
       e.target.parentElement.remove()
@@ -77,23 +51,47 @@ function addDltBtn(taskEl) {
   })
 }
 
-// 데이터를 JSON 형식으로 저장
+function currentDate() {
+  const date = new Date()
+
+  const year = date.getFullYear() // 연도
+  const month = date.getMonth() + 1 // 월은 0부터 시작하므로 1을 더해줌
+  const day = date.getDate() // 일
+
+  const formattedDate = `${year}년 ${month}월 ${day}일`
+
+  const dateEl = document.querySelector('.todo-date')
+  const addDate = document.createElement('span')
+  addDate.textContent = formattedDate
+  dateEl.appendChild(addDate)
+}
+currentDate()
+
 function saveData() {
+  const tasks = listEl.querySelectorAll('li')
+  const tasksArray = Array.from(tasks)
   const data = {
-    listHTML: listEl.innerHTML,
-    otherData: 'some other data',
-    // ... 추가적인 데이터 저장
+    tasks: tasksArray.map((task) => ({
+      content: task.textContent,
+      isChecked: task.classList.contains('checked'),
+    })),
   }
   localStorage.setItem('data', JSON.stringify(data))
 }
 
-// JSON 데이터를 가져와서 화면에 보여주기
 function showTask() {
   const data = JSON.parse(localStorage.getItem('data'))
-  if (data) {
-    listEl.innerHTML = data.listHTML
-    // 다른 데이터도 복원할 수 있음
-    // someOtherElement.textContent = data.otherData;
+  if (data && data.tasks) {
+    data.tasks.forEach((task) => {
+      const taskEl = document.createElement('li')
+      taskEl.textContent = task.content
+      if (task.isChecked) {
+        taskEl.classList.add('checked')
+      }
+      listEl.appendChild(taskEl)
+      CtlCheckbox(taskEl)
+      addDltBtn(taskEl)
+    })
   }
 }
 
