6d3f4aa029591641b4a90150f535f974e090c871
imnayoung <myblu1008@gmail.com>
2023-08-01 09:57:24 +0900

    Feat: Add Editing Feat

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-const inputEl = document.getElementById('input')
+const aInputEl = document.getElementById('input')
 const addBtnEl = document.getElementById('addBtn')
 const listEl = document.querySelector('.todo-list')
 
 addBtnEl.addEventListener('click', addTask)
 
-inputEl.addEventListener('keydown', (e) => {
+aInputEl.addEventListener('keydown', (e) => {
   if (e.key === 'Enter') {
     if (e.isComposing) return // keydown 이슈방지
     addTask()
@@ -12,19 +12,20 @@ inputEl.addEventListener('keydown', (e) => {
 })
 
 function addTask() {
-  if (inputEl.value === '') {
+  if (aInputEl.value === '') {
     alert('문자를 입력해주세요!')
     return
   }
 
   const taskEl = document.createElement('li')
   const taskTxt = document.createElement('span')
-  taskTxt.textContent = inputEl.value
+  taskTxt.textContent = aInputEl.value
   listEl.appendChild(taskEl)
   taskEl.appendChild(taskTxt)
-  inputEl.value = null
+  aInputEl.value = null
 
-  addDltBtn(taskEl)
+  addEditBtn(taskEl) // editing 버튼 추가
+  addDltBtn(taskEl) // remove 버튼 추가
   CtlCheckbox(taskEl)
   saveData()
 }
@@ -38,12 +39,77 @@ function CtlCheckbox(taskEl) {
   })
 }
 
-function addDltBtn(taskEl) {
-  let xIcon = document.createElement('i')
-  xIcon.setAttribute('class', 'xi-close')
-  taskEl.append(xIcon)
+/**
+ * Editing
+ */
+function addEditBtn(taskEl) {
+  let eXIcon = document.createElement('i') // 변수명을 eXIcon으로 변경
+  eXIcon.setAttribute('class', 'xi-pen')
+  taskEl.append(eXIcon)
 
-  xIcon.addEventListener('click', (e) => {
+  eXIcon.addEventListener('click', () => {
+    editTodoItem(taskEl)
+  })
+}
+
+function editTodoItem(item) {
+  // 이미 수정 중인 항목이면 리턴
+  if (item.classList.contains('editing')) {
+    return
+  }
+
+  const existingSpan = item.querySelector('span')
+  const oldText = existingSpan ? existingSpan.textContent : ''
+
+  const eInputEl = document.createElement('input')
+  eInputEl.value = oldText
+
+  if (existingSpan) {
+    existingSpan.style.display = 'none' // 기존의 span 태그를 일시적으로 숨김
+    item.insertBefore(eInputEl, existingSpan) // input 태그를 span 태그 앞에 삽입
+  } else {
+    item.appendChild(eInputEl) // span 태그가 없으면 그냥 input 태그를 추가
+  }
+
+  item.classList.add('editing')
+  eInputEl.focus()
+
+  // 수정 완료 이벤트 리스너 등록
+  eInputEl.addEventListener('blur', () => {
+    finishEditing(item, eInputEl.value)
+  })
+
+  eInputEl.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+      if (e.isComposing) return
+      finishEditing(item, eInputEl.value)
+    }
+  })
+
+  function finishEditing(item, newText) {
+    if (existingSpan) {
+      existingSpan.textContent = newText
+      existingSpan.style.display = '' // 텍스트 수정이 끝나면 다시 span 태그 보이도록 설정
+    } else {
+      const newSpan = document.createElement('span')
+      newSpan.textContent = newText
+      item.insertBefore(newSpan, eInputEl) // input 태그를 삭제하고 span 태그를 추가
+    }
+
+    item.classList.remove('editing')
+    eInputEl.remove() // input 태그 삭제
+  }
+}
+
+/**
+ * Remove
+ */
+function addDltBtn(taskEl) {
+  let dXIcon = document.createElement('i') // 변수명을 dXIcon으로 변경
+  dXIcon.setAttribute('class', 'xi-close')
+  taskEl.append(dXIcon)
+
+  dXIcon.addEventListener('click', (e) => {
     if (e.target.tagName === 'I') {
       e.target.parentElement.remove()
       saveData()
@@ -71,10 +137,13 @@ function saveData() {
   const tasks = listEl.querySelectorAll('li')
   const tasksArray = Array.from(tasks)
   const data = {
-    tasks: tasksArray.map((task) => ({
-      content: task.textContent,
-      isChecked: task.classList.contains('checked'),
-    })),
+    tasks: tasksArray.map((task) => {
+      const taskData = {
+        content: task.querySelector('span') ? task.querySelector('span').textContent : task.textContent,
+        isChecked: task.classList.contains('checked'),
+      }
+      return taskData
+    }),
   }
   localStorage.setItem('data', JSON.stringify(data))
 }
@@ -84,15 +153,18 @@ function showTask() {
   if (data && data.tasks) {
     data.tasks.forEach((task) => {
       const taskEl = document.createElement('li')
-      taskEl.textContent = task.content
+      const span = document.createElement('span')
+      span.textContent = task.content
+
       if (task.isChecked) {
         taskEl.classList.add('checked')
       }
+
+      taskEl.appendChild(span)
       listEl.appendChild(taskEl)
       CtlCheckbox(taskEl)
+      addEditBtn(taskEl)
       addDltBtn(taskEl)
     })
   }
 }
-
-showTask()
